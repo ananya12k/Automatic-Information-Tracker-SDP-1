117f326136450b31df71568609585a323d0a577e
Setu Bhatt <bansetu1@gmail.com>
2024-02-16 15:20:08 +0530

    created card component common for Hostel and PG & created instance of it in hostel

--- frontend/src/pages/HostelGirls.jsx
+++ frontend/src/pages/HostelGirls.jsx
@@ -1,5 +1,14 @@
+import Card from "../components/Card";
+
 const HostelGirls = () => {
-  return <div>HostelGirls</div>;
+  return (
+    <>
+      {/* Later on, this data will come from database */}
+      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostels's location" phoneNumber="Hostel's phone" />
+      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostels's location" phoneNumber="Hostel's phone" />
+      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostels's location" phoneNumber="Hostel's phone" />
+    </>
+  );
 };
 
 export default HostelGirls;
