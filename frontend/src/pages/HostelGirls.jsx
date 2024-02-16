749003c44064555ba00021a983a6523b0c2718fb
Setu Bhatt <bansetu1@gmail.com>
2024-02-16 16:38:51 +0530

    created a common navbar for hostel, pg and tiffin

--- frontend/src/pages/HostelGirls.jsx
+++ frontend/src/pages/HostelGirls.jsx
@@ -1,12 +1,14 @@
 import Card from "../components/Card";
+import NavBarServices from "../components/NavBarServices";
 
 const HostelGirls = () => {
   return (
     <>
+      <NavBarServices />
       {/* Later on, this data will come from database */}
-      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostels's location" phoneNumber="Hostel's phone" />
-      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostels's location" phoneNumber="Hostel's phone" />
-      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostels's location" phoneNumber="Hostel's phone" />
+      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostel's Location" phoneNumber="Hostel's Phone" />
+      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostel's Location" phoneNumber="Hostel's Phone" />
+      <Card name="Hostel's Name" amenities="Hostel's Amentities" address="Hostel's Location" phoneNumber="Hostel's Phone" />
     </>
   );
 };
