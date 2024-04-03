84c8283950b6a80592d09630aff05119eb7c38ef
Setu Bhatt <bansetu1@gmail.com>
2024-04-03 16:43:32 +0530

    card completed with backdrop functionality to blur background when modal is open

---
+++ frontend/src/components/AddressCard.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBBtn
+} from 'mdb-react-ui-kit';
+import ReviewModal from './ReviewModal';
+import Backdrop from '../components/Backdrop';
+
+const AddressCard = ({ hostel }) => {
+    const [showReviewModal, setShowReviewModal] = useState(false);
+
+    const openOnGoogleMap = () => {
+        const latitude = hostel.latitude;
+        const longitude = hostel.longitude;
+        const googleMapsUrl = `http://maps.google.com/maps?q=${latitude},${longitude}`;
+        window.location.href = googleMapsUrl;
+    }
+
+    const openReviewModal = () => {
+        setShowReviewModal(true);
+    }
+
+    return (
+        <>
+            <MDBCard>
+                <MDBCardBody>
+                    <div style={{ textAlign: 'left' }}>
+                        <MDBCardTitle>Address</MDBCardTitle>
+                        <MDBCardText>
+                            {hostel ? hostel.address : "Address Not Available"}
+                        </MDBCardText>
+                        <MDBBtn onClick={openOnGoogleMap}>Get Directions</MDBBtn>
+                    </div>
+                </MDBCardBody>
+            </MDBCard>
+
+            <MDBCard style={{ marginTop: "10px" }}>
+                <MDBCardBody>
+                    <div style={{ textAlign: 'left' }}>
+                        <MDBCardTitle>Phone Number</MDBCardTitle>
+                        <MDBCardText>
+                            {hostel ? hostel.phone : "Phone Number Not Available"}
+                        </MDBCardText>
+                    </div>
+                </MDBCardBody>
+            </MDBCard>
+
+            <MDBCard style={{ marginTop: "10px" }}>
+                <MDBCardBody>
+                    <div style={{ textAlign: 'left' }}>
+                        <MDBCardTitle>Want to add your review?</MDBCardTitle>
+                        <MDBBtn onClick={openReviewModal}>Write Review</MDBBtn>
+                    </div>
+                </MDBCardBody>
+            </MDBCard>
+
+            <Backdrop show={showReviewModal} />
+
+            <ReviewModal isOpen={showReviewModal} setOpen={setShowReviewModal} />
+        </>
+    )
+}
+
+export default AddressCard;
--- frontend/src/components/ReviewModal.jsx
+++ frontend/src/components/ReviewModal.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import {
   MDBBtn,
@@ -10,69 +11,63 @@ import {
   MDBModalFooter,
   MDBCard,
   MDBCardBody,
-  MDBContainer
+ MDBContainer
 } from 'mdb-react-ui-kit';
 import Rating from './Rating';
 import UserInput from './UserInput';
 import UserProfile from './UserProfile';
 
-export default function ReviewModal() {
-  const [basicModal, setBasicModal] = useState(false);
-  const [selectedPhotoName, setSelectedPhotoName] = useState('');
+export default function ReviewModal({ isOpen, setOpen }) {
+ const [selectedPhotoName, setSelectedPhotoName] = useState('');
 
-  const toggleOpen = () => setBasicModal(!basicModal);
-
-  const handleFileChange = (event) => {
+ const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
       setSelectedPhotoName(file.name);
     }
-  };
+ };
 
-  const triggerFileSelect = () => {
+ const triggerFileSelect = () => {
     document.getElementById('upload-photo').click();
-  };
+ };
 
-  return (
-    <>
-      <MDBBtn onClick={toggleOpen} color='primary'>LAUNCH DEMO MODAL</MDBBtn>
-      <MDBModal open={basicModal} setOpen={setBasicModal} tabIndex='-1'>
-        <MDBModalDialog centered scrollable size='lg'>
-          <MDBModalContent>
-            <MDBModalHeader>
-              <MDBModalTitle className='text-center'>Hostel Name</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
-            </MDBModalHeader>
-            <MDBModalBody style={{ overflowY: 'scroll', scrollbarWidth: 'none', msOverflowStyle: 'none', maxHeight: '400px' }}>
-              <MDBContainer className='d-flex flex-column align-items-center'>
-                <UserProfile />
-                <Rating />
-                <UserInput styles={{ width: '100%', marginTop: '20px' }} />
-                {selectedPhotoName ? (
-                  <p onClick={triggerFileSelect} style={{ cursor: 'pointer', color: 'blue' }}>
-                    {selectedPhotoName}
-                  </p>
-                ) : (
-                  <label htmlFor="upload-photo" className="btn btn-primary" style={{ marginTop: '10px' }}>
-                    Upload Photos
-                  </label>
-                )}
-                <input
-                  type="file"
-                  name="photo"
-                  id="upload-photo"
-                  style={{ display: 'none' }}
-                  onChange={handleFileChange}
-                  accept="image/jpeg, image/png, image/gif"
-                />
-              </MDBContainer>
-            </MDBModalBody>
-            <MDBModalFooter>
-              <MDBBtn color='secondary' onClick={toggleOpen}>Submit Review</MDBBtn>
-            </MDBModalFooter>
-          </MDBModalContent>
-        </MDBModalDialog>
-      </MDBModal>
-    </>
-  );
+ return (
+    <MDBModal open={isOpen} setOpen={setOpen} tabIndex='-1'>
+      <MDBModalDialog centered scrollable size='lg'>
+        <MDBModalContent>
+          <MDBModalHeader>
+            <MDBModalTitle className='text-center'>Hostel Name</MDBModalTitle>
+            <MDBBtn className='btn-close' color='none' onClick={() => setOpen(false)}></MDBBtn>
+          </MDBModalHeader>
+          <MDBModalBody style={{ overflowY: 'scroll', scrollbarWidth: 'none', msOverflowStyle: 'none', maxHeight: '400px' }}>
+            <MDBContainer className='d-flex flex-column align-items-center'>
+              <UserProfile />
+              <Rating />
+              <UserInput styles={{ width: '100%', marginTop: '20px' }} />
+              {selectedPhotoName ? (
+                <p onClick={triggerFileSelect} style={{ cursor: 'pointer', color: 'blue' }}>
+                 {selectedPhotoName}
+                </p>
+              ) : (
+                <label htmlFor="upload-photo" className="btn btn-primary" style={{ marginTop: '10px' }}>
+                 Upload Photos
+                </label>
+              )}
+              <input
+                type="file"
+                name="photo"
+                id="upload-photo"
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+                accept="image/jpeg, image/png, image/gif"
+              />
+            </MDBContainer>
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn color='secondary' onClick={() => setOpen(false)}>Submit Review</MDBBtn>
+          </MDBModalFooter>
+        </MDBModalContent>
+      </MDBModalDialog>
+    </MDBModal>
+ );
 }
