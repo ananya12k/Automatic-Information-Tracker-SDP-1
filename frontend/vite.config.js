11b6d2861353fb3009324d938618f6e5ed38b951
Setu Bhatt <bansetu1@gmail.com>
2024-04-05 09:09:15 +0530

    Revert "deployment config"
    This reverts commit cf64cba8855e4d29a202b424c514bef4f164a8c6.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Automatic-Information-Tracker-SDP-1/frontend/",
+  base: "/Automatic-Information-Tracker-SDP-1/",
 });
